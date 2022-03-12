95faf296829050a5cfe7b7a07fad7a0ad0662d19
jss2a98aj <jss2a98aj@gmail.com>
2022-03-11 21:43:25 -0500

    Made versioning work entirely off of git tags

---
+++ src/main/java/jss/bugtorch/core/Tags.java
@@ -0,0 +1,9 @@
+package jss.bugtorch.core;
+
+// Use this class for Strings only. Do not import any classes here. It will lead to issues with Mixins if in use!
+
+public class Tags {
+
+    // GRADLETOKEN_* will be replaced by your configuration values at build timeME";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
+}
