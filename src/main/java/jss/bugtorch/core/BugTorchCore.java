2a526e5c46398045f48e3fb37b4e7d424c4a90bd
jss2a98aj <jss2a98aj@gmail.com>
2023-02-11 22:51:54 -0500

    Removed LoadedMods util
    It was unneeded in the long term despite previous assumptions.

--- src/main/java/jss/bugtorch/core/BugTorchCore.java
+++ src/main/java/jss/bugtorch/core/BugTorchCore.java
@@ -2,18 +2,18 @@ package jss.bugtorch.core;
 
 import java.io.File;
 
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import jss.bugtorch.config.BugTorchConfig;
 import jss.bugtorch.modsupport.PamsTemperatePlantsSupport;
 import jss.bugtorch.modsupport.ThaumcraftSupport;
 import jss.bugtorch.modsupport.VillageNamesSupport;
 import jss.bugtorch.modsupport.WitcherySupport;
-import jss.bugtorch.util.LoadedMods;
 
 @Mod(
 		modid = BugTorchCore.MODID,
@@ -29,29 +29,30 @@ public class BugTorchCore {
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
-	@EventHandler
+	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		String configFolder =  event.getModConfigurationDirectory().getAbsolutePath() + File.separator + MODID + File.separator;
 		BugTorchConfig.loadBaseConfig(new File(configFolder + "base.cfg"));
 		BugTorchConfig.loadModdedConfig(new File(configFolder + "modSupport.cfg"));
 
-		LoadedMods.detectLoadedMods();
-
 		VanillaSupport.enableSupport();
+	}
 
-		if(LoadedMods.pamsTemperatePlantsLoaded) {
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		if(Loader.isModLoaded("temperateplants")) {
 			PamsTemperatePlantsSupport.enableSupport();
 		}
 
-		if(LoadedMods.thaumcraftLoaded) {
+		if(Loader.isModLoaded("Thaumcraft")) {
 			ThaumcraftSupport.enableSupport();
 		}
 
-		if(LoadedMods.witcheryLoaded) {
+		if(Loader.isModLoaded("witchery")) {
 			WitcherySupport.enableSupport();
 		}
 
-		if(LoadedMods.villageNamesLoaded) {
+		if(Loader.isModLoaded("VillageNames")) {
 			VillageNamesSupport.enableSupport();
 		}
 	}
