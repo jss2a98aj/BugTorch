de02eb19c689859b3a88c40949414e1f413ac1bb
jss2a98aj <jss2a98aj@gmail.com>
2023-03-20 17:16:26 -0400

    Fix anvil sound type step sound outside of dev

--- src/main/java/jss/bugtorch/mixins/early/minecraft/fix/block/MixinSoundTypeAnvil.java
+++ src/main/java/jss/bugtorch/mixins/early/minecraft/fix/block/MixinSoundTypeAnvil.java
@@ -1,12 +1,18 @@
 package jss.bugtorch.mixins.early.minecraft.fix.block;
 
+import net.minecraft.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(targets = "net/minecraft/block/Block$3")
-public abstract class MixinSoundTypeAnvil {
+public abstract class MixinSoundTypeAnvil extends Block.SoundType {
 
+    @Override
     public String getStepResourcePath() {
         return "step.stone";
+    }
+
+    MixinSoundTypeAnvil(String name, float volume, float frequency) {
+        super(name, volume, frequency);
     }
 
 }
