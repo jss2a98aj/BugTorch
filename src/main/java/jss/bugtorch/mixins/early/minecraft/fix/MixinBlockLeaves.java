d701a7f73d7149fd4388f51d9b84e6d4fda0c22a
roadhog360 <11903511+Roadhog360@users.noreply.github.com>
2025-01-21 03:14:33 -0600

    Remove MixinBlockLeaves conditions
    MixinBlockLeaves condition removal may or may not be temporary, might make it into a chain of if statements.

--- src/main/java/jss/bugtorch/mixins/early/minecraft/fix/MixinBlockLeaves.java
+++ src/main/java/jss/bugtorch/mixins/early/minecraft/fix/MixinBlockLeaves.java
@@ -1,20 +1,11 @@
 package jss.bugtorch.mixins.early.minecraft.fix;
 
-import java.util.IdentityHashMap;
-import java.util.Map;
-
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.init.Blocks;
-import net.minecraftforge.oredict.OreDictionary;
-import org.apache.commons.lang3.tuple.Pair;
-import org.spongepowered.asm.mixin.Mixin;
-
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.block.BlockLeavesBase;
 import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -24,17 +15,14 @@ public abstract class MixinBlockLeaves extends BlockLeavesBase {
         super(p_i45433_1_, p_i45433_2_);
     }
 
-    @SuppressWarnings("EqualsBetweenInconvertibleTypes")
     @ModifyVariable(method = "updateTick", at = @At(value = "STORE"), ordinal = 0)
     private byte editLeafDecayVal(byte value,
                                   @Local(argsOnly = true) World world,
                                   @Local(argsOnly = true, ordinal = 0) int x,
                                   @Local(argsOnly = true, ordinal = 1) int y,
-                                  @Local(argsOnly = true, ordinal = 2) int z,
-                                  @Local(ordinal = 3) int meta) {
-        meta &= 3;
-        if(this.equals(Blocks.leaves) && meta == 3) { //Jungle leaves
-            return 6;
+                                  @Local(argsOnly = true, ordinal = 2) int z) {
+        if(value == 4) { //Default value
+            return 7;
         }
         return value;
     }
